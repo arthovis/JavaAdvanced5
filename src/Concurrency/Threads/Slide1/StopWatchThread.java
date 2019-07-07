33fbf8968f6566e5a270d43c660eb0e58ef02e2a
Andrei <andreifbaicu@gmail.com>
2019-07-07 15:25:37 +0300

    cate ceva

--- src/Concurrency/Threads/Slide1/StopWatchThread.java
+++ src/Concurrency/Threads/Slide1/StopWatchThread.java
@@ -1,16 +1,15 @@
 package Concurrency.Threads.Slide1;
 
-public class StopWatchThread extends Thread{
+public class StopWatchThread extends Thread {
     @Override
-    public void run(){
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Stop Watch: "+i);
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Stop Watch: " + i);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
     }
 }
