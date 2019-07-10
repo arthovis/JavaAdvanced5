905f8614f3eda36839e10880d330b990d64a6a24
Andrei <andreifbaicu@gmail.com>
2019-07-10 20:31:18 +0300

    Added some lines to IO/Exercitiu1/DemoExercitiu1

--- src/IO/Exercitiu1/DemoExercitiu1.java
+++ src/IO/Exercitiu1/DemoExercitiu1.java
@@ -1,6 +1,6 @@
 package IO.Exercitiu1;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,16 +8,16 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class DemoExercitiu1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Path source = Paths.get("simplefile.txt");
         System.out.println(countWordsJava8(source));
 
 //        long result = countWordsJava8(source);
 //        System.out.println(result);
-
+        countWordsJava7(source);
     }
 
-    private static long countWordsJava8(Path path){
+    private static long countWordsJava8(Path path) {
         long count = 0;
         //read lines from path
         try {
@@ -34,8 +34,26 @@ public class DemoExercitiu1 {
         return count;
     }
 
-    private static long countWordsJava7(Path path){
+    private static void countWordsJava7(Path path) throws IOException {
+
         //TODO: implement usingg Java 7
-        return 0;
+        FileReader fileReader = new FileReader("C:\\Users\\Andrei\\IdeaProjects\\Java Advanced 5\\simplefile.txt");
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileReader))) {
+            //read line by line
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                long count = 0;
+                String[] a = line.split(" ");
+                for (int i = 0; i < a.length; i++) {
+                    if (a[i].length() > 0) {
+                        count++;
+                    }
+                    line = bufferedReader.readLine();
+                    System.out.println("Total number of words: " + count);
+                    bufferedReader.close();
+                }
+            }
+        }
     }
 }
